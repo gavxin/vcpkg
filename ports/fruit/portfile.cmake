50ed3b89bba4d613e2a300c9af917203118729a5
gavxin <gavxin@users.noreply.github.com>
2018-12-01 16:30:19 +0800

    Fix fruit package not contain fruit.dll error.

--- ports/fruit/portfile.cmake
+++ ports/fruit/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REF v3.4.0
     SHA512 d78c76432c77acc4cc6ccf3fd9627a3fb2a0aa55d1baf7346422e9f1c1e048237d136588b44cfa943b542b43adbbb62fcd524e4a1cb870e9ffe8b7cf4dadb35d
     HEAD_REF master
+    PATCHES
+        fix_no_dll.patch
 )
 
 # TODO: Make boost an optional dependency?
